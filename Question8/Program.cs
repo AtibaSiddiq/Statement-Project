13967210275b8c919c55639165863b50f79f661b
SBM2009 <atibasiddiq@gmail.com>
2022-08-17 18:54:01 +0100

    Conditional statement assessment

--- Question8/Program.cs
+++ Question8/Program.cs
@@ -7,12 +7,16 @@ namespace Question8
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a number");
-            int num1  = int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter a number");
-            int num2  = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter a number");
-            int num3  = int.Parse(Console.ReadLine());
-            if(num1 < 0 && num2 < 0 || num2 < 0 && num3 < 0 || num3 < 0 && num1 < 0)
+            int num3 = int.Parse(Console.ReadLine());
+            if(num1 < 0 && num2 < 0 && num3 < 0)
+            {
+                System.Console.WriteLine("-");
+            }
+            else if(num1 < 0 && num2 < 0 || num2 < 0 && num3 < 0 || num3 < 0 && num1 < 0)
             {
                 System.Console.WriteLine("+");
             }
@@ -24,7 +28,10 @@ namespace Question8
             {
                 System.Console.WriteLine("-");
             }
-            else if(num1 < 0 && num2 > 0 || num)
+            else if(num1 < 0 && num2 > 0 && num3 > 0 || num2 < 0 && num1 > 0 && num3 > 0 || num3 < 0 && num2 > 0 && num1 > 0)
+            {
+                System.Console.WriteLine("-");
+            }
         }
     }
 }
